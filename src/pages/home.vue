716333b094d87de7c11ddb3f852ca90ac78ac3fc
1156657702@qq.com <1156657702@qq.com>
2022-02-08 22:54:49 +0800

    asdfasdfasd

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,10 @@
 <template>
     <div>这是首页</div>
-    <todo-list></todo-list>
+    <!-- <todo-list></todo-list> -->
+    <count></count>
 </template>
 <script setup>
 import TodoList from '../components/TodoList.vue';
+import Count from '../components/Count.vue'
 
 </script> 
