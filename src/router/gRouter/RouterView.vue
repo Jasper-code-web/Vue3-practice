aed0cadee108a0a70f68e7b55463375c0576818c
Jasper-code-web <1156657702@qq.com>
2022-07-06 17:59:32 +0800

    router原理

---
+++ src/router/gRouter/RouterView.vue
@@ -0,0 +1,16 @@
+<template>
+    <component :is="comp"></component>
+</template>
+<script setup>
+import {computed} from 'vue'
+import {useRouter} from './index'
+
+let router = useRouter()
+
+const comp = computed(() => {
+    let route = router.routes.find((route) => route.path === router.current)
+    return route ? route.component : null
+})
+console.log(comp)
+
+</script>
